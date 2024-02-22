aa25f04833e5541f0d594fd6af12bd051984ee8e
somy10741 <somy10741@gmail.com>
2024-02-22 21:12:19 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Somesh !!!! Welecome C# Learning");
+            Console.ReadLine();
+            Console.Beep();
+            Console.WriteLine("I will give one commitment to myself i.e. I will learn basic C#");
+            Console.Beep();
+            Console.ReadLine();
+
+            //Variables and datatypes
+            //declaring a variable
+            int num1;
+
+            //Assign a value to variable
+            num1 = 60;
+            int num2 = 33;
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+            Console.Read();
+
+        }
+    }
+}
