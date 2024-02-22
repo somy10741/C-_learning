cf63308b3a98e16ee493cf0b4e6103cdd65604ad
somy10741 <somy10741@gmail.com>
2024-02-22 21:45:29 +0530

    new changes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,22 +10,35 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Somesh !!!! Welecome C# Learning");
-            Console.ReadLine();
-            Console.Beep();
-            Console.WriteLine("I will give one commitment to myself i.e. I will learn basic C#.");
-            Console.Beep();
-            Console.ReadLine();
+            //Console.WriteLine("Hello Somesh !!!! Welecome C# Learning");
+            //Console.ReadLine();
+            //Console.Beep();
+            //Console.WriteLine("I will give one commitment to myself i.e. I will learn basic C#.");
+            //Console.Beep();
+            //Console.ReadLine();
 
-            //Variables and datatypes
-            //declaring a variable
-            int num1;
+            ////Variables and datatypes
+            ////declaring a variable
+            //int num1;
 
-            //Assign a value to variable
-            num1 = 60;
-            int num2 = 33;
-            int sum = num1 + num2;
-            Console.WriteLine(sum);
+            ////Assign a value to variable
+            //num1 = 60;
+            //int num2 = 33;
+            //int sum = num1 + num2;
+            //Console.WriteLine(sum);
+            //Console.Read();
+
+            string name = "Somesh Kumar Sahu";
+            Console.WriteLine("My name is " + name);
+
+            //Convert the string to all UPPERCASE
+            string nameUpper = name.ToUpper();
+            Console.WriteLine("My name is " + nameUpper);
+
+            //Convert the string to all lowercase
+            string nameLower = name.ToLower();
+            Console.WriteLine("My name is " + nameLower);
+
             Console.Read();
 
         }
