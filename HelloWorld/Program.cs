801aa642beaf026a67150e828c5f04805c484d66
somy10741 <somy10741@gmail.com>
2024-02-27 00:19:59 +0530

    newly added

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -107,24 +107,73 @@ namespace HelloWorld
             //String manipulation
             //1. String concatination.
             //Declaring variable
-            int age = 32;
-            string name = "Somesh Kumar Sahu";
-            string companyName = "ZF India Pvt. Ltd. Hyderabad";
-            Console.WriteLine("String concatination");
-            Console.WriteLine("My name is " + name + " and My age is " + age + ". Currently I am working in " + companyName);
+            //int age = 32;
+            //string name = "Somesh Kumar Sahu";
+            //string companyName = "ZF India Pvt. Ltd. Hyderabad";
+            //Console.WriteLine("String concatination");
+            //Console.WriteLine("My name is " + name + " and My age is " + age + ". Currently I am working in " + companyName);
 
-            //2. String formatting :- It is used with the help of indexes
-            Console.WriteLine("String formating");
-            Console.WriteLine("My name is {0} and My age is {1}. Currently I am working in {2}",name,age,companyName);
+            ////2. String formatting :- It is used with the help of indexes
+            //Console.WriteLine("String formating");
+            //Console.WriteLine("My name is {0} and My age is {1}. Currently I am working in {2}",name,age,companyName);
 
-            //3. String interpolation :- It uses $ at the start which will allow us to write variables like {variableName}
-            Console.WriteLine("String interpolation");
-            Console.WriteLine($"My name is {name} and My age is {age}. Currently I am working in {companyName}");
+            ////3. String interpolation :- It uses $ at the start which will allow us to write variables like {variableName}
+            //Console.WriteLine("String interpolation");
+            //Console.WriteLine($"My name is {name} and My age is {age}. Currently I am working in {companyName}");
 
-            //4. Verbatim strings :- instead of using \\ to write paths we can use verbatim strings to make it easier
-            // if you remove the @ you will get an error because \U, \A and \D are not valid escape characters
-            Console.WriteLine("verbatim strings");
-            Console.WriteLine(@"F:\C# COurse\HelloWorld\Sahu.png");
+            ////4. Verbatim strings :- instead of using \\ to write paths we can use verbatim strings to make it easier
+            //// if you remove the @ you will get an error because \U, \A and \D are not valid escape characters
+            //Console.WriteLine("verbatim strings");
+            //Console.WriteLine(@"F:\C# COurse\HelloWorld\Sahu.png");
+
+            //-----------------------------------------------------------------------------------------------------------
+
+            //*********** Various String Methods **************
+            //--> SubString(Int32) : It is used to get the substring from the string, starting from the specified index.
+            //--> ToLower() : It is used to convert the string to lowercase.
+            //--> ToUpper() : It is used to convert the string to uppercase.
+            //--> Trim() : It is used to trim all leading and trailing white space from the string.
+            //--> IndexOf() : It is used to get the first occurence of the string or character inside the string
+            //--> IsNullOrWhiteSpace : It returns true if the string is either null or is blank else it returns false
+
+            //string firstName = "Somesh";
+            //string lastName = "Kumar";
+            //string fullName = string.Concat(" ", firstName, lastName, " ");
+            //Console.WriteLine(fullName);
+            //Console.WriteLine(firstName.Substring(2));
+            //Console.WriteLine(firstName.ToLower());
+            //Console.WriteLine(firstName.ToUpper());
+            //Console.WriteLine(fullName.Trim());
+            //Console.WriteLine(firstName.IndexOf('m'));
+            //Console.WriteLine(String.IsNullOrWhiteSpace(firstName));
+            //Console.WriteLine(String.Format($"My name is {fullName}"));
+
+            /*
+             Challenge 1 – String and its methods
+             --> Now you know the use of various string functions so let’s create a small Console application 
+             to practice this.
+             --> Declare a string variable and don’t assign any value to it.
+             --> Print on the console “Please enter your name and press enter”. You can then enter your name 
+             or any other valid string like “tutorials.eu”.
+             --> Assign that entered string to the string variable which you have declared initially.
+             --> The program should write on the console that string in Uppercase in the first line, 
+             then the same string in Lowercase in the second line. In the third line, it writes on the 
+             console the string with no trailing or preceding white space like if string entered as “ tutorials.eu ” 
+             it should be written on the console as “tutorials.eu”. And in the last line, it should write the 
+             Substring of the entered string on the console.
+             --> These kinds of features are e.g. used when creating a login screen, where the system 
+             will cut out trailing or preceding white space or, doesn’t care about the casing of the 
+             entered username.
+             */
+            // Solution.
+            string myName;
+            Console.Write("Please enter your name and then press enter : ");
+            myName = Console.ReadLine();
+            Console.WriteLine($"You entered name is {myName}");
+            Console.WriteLine($"Upper case : {myName.ToUpper()}");
+            Console.WriteLine($"Lower case : {myName.ToLower()}");
+            Console.WriteLine($"Trim : {myName.Trim()}");
+            Console.WriteLine($"Substring : {myName.Substring(3,8)}");
             Console.ReadKey();
         }
     }
