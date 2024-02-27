04596a6a8c5cf50d7a89a7d3da2532672794e793
somy10741 <somy10741@gmail.com>
2024-02-28 00:06:40 +0530

    latest change

---
+++ HelloWorld/Function_and_method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Function_and_method
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
