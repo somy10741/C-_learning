6a2c95dbe713877e201c744c4ae7ca4301d8d06a
somy10741 <somy10741@gmail.com>
2024-03-04 21:59:27 +0530

    new project added

---
+++ Making_Decisions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Making_Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Somesh! Lets learn making decisions");
+            Console.ReadKey();
+        }
+    }
+}
