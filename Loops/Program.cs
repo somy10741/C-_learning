8e9b47d869371c2085dcd9ca342867eb89dc0c30
somy10741 <somy10741@gmail.com>
2024-03-24 21:41:20 +0530

    completed

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -58,7 +58,41 @@ namespace Loops
                 teacher enters an incorrect value.
                 Test your program thoroughly.
              */
+            // Solution
+            string input = "0";
+            int count = 0;
+            int total = 0;
+            int currentNumber = 0;
 
+            while (input != "-1")
+            {
+                Console.WriteLine("Last number was {0}", currentNumber);
+                Console.WriteLine("Please enter the next score");
+                Console.WriteLine("Current amount of entries {0}", count);
+                Console.WriteLine("Please enter -1 once you are ready to calculate the average");
+
+                input = Console.ReadLine();
+                if (input.Equals("-1"))
+                {
+                    Console.WriteLine("--------------------------------------------------");
+                    // Calculate the average and let the teacher know
+                    double average = (double)total / (double)count;
+                    Console.WriteLine("The average score of students is {0}", average);
+                }
+                if(int.TryParse(input, out currentNumber) && currentNumber > 0 && currentNumber <= 20)
+                {
+                    total += currentNumber;
+                }
+                else
+                {
+                    if (!input.Equals("-1"))
+                    {
+                        Console.WriteLine("Please enter a value between 1 and 20");
+                    }
+                    continue;
+                }
+                count++;
+            }
 
 
             Console.Read();
