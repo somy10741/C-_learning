1ee6f8614265a7deeea026c256427f2f7e315a00
somy10741 <somy10741@gmail.com>
2024-04-24 22:16:54 +0530

    new

---
+++ OOPs/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs
+{
+    class Car
+    {
+        // Constructor
+        public Car()
+        {
+            Console.WriteLine("Car was created");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Car is Driving");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Car Stopped");
+        }
+    }
+}
---
+++ OOPs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Somesh ! Welcome to OOPs Lecture");
+            // Class :- A class is blue print of an object.
+            //          It has actions/abilities so called member functions or methods
+            //          and it has properties so called member variables
+            //          Inheritance possible
+            Car audi = new Car();
+            audi.Drive();
+            Console.WriteLine("Press 1 to stop the car!");
+            string userInput = Console.ReadLine();
+            if(userInput == "1")
+            {
+                audi.Stop();
+            }
+            else
+            {
+                Console.WriteLine("Car drives indefinitely");
+            }
+            Console.ReadKey();
+        }
+    }
+}
