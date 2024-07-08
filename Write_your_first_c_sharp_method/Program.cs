4b08ac0c9aa3ffe96444344aaca31beca169773a
somy10741 <somy10741@gmail.com>
2024-07-08 23:17:41 +0530

    new

---
+++ Write_your_first_c_sharp_method/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write_your_first_c_sharp_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+
+            void PrintArray()
+            {
+                foreach (int x in a)
+                {
+                    Console.Write($"{x} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Contents of Array:");
+            PrintArray();
+            Console.ReadKey();
+
+            
+        }
+    }
+}
